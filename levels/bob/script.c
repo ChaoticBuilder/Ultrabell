e1c658a0063a6733f63c7cee05818e6f128c22c7
ChaoticBuilder <lewkat_2008@outlook.com>
2025-06-18 21:17:05 -0600

    plains overhaul

--- levels/bob/script.c
+++ levels/bob/script.c
@@ -67,6 +67,7 @@ const LevelScript level_bob_entry[] = {
 		TERRAIN(bob_area_1_collision),
 		MACRO_OBJECTS(bob_area_1_macro_objs),
 		SET_BACKGROUND_MUSIC(0x00, SEQ_LEVEL_GRASS),
+		SHOW_DIALOG(0x00, DIALOG_000),
 		TERRAIN_TYPE(TERRAIN_GRASS),
 		/* Fast64 begin persistent block [area commands] */
 		/* Fast64 end persistent block [area commands] */
--- levels/bob/texture_include.inc.c
+++ levels/bob/texture_include.inc.c
@@ -1,11 +1,11 @@
-Gfx bob_dl_bob_textures_05800_rgba16_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 bob_dl_bob_textures_05800_rgba16_rgba16[] = {
-	#include "levels/bob/bob_textures.05800.rgba16.inc.c"
+Gfx bob_dl_grass1_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_grass1_rgba16[] = {
+	#include "levels/bob/grass1.rgba16.inc.c"
 };
 
-Gfx bob_dl_bob_textures_09000_rgba16_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 bob_dl_bob_textures_09000_rgba16_rgba16[] = {
-	#include "levels/bob/bob_textures.09000.rgba16.inc.c"
+Gfx bob_dl_dirt1fx_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_dirt1fx_rgba16[] = {
+	#include "levels/bob/dirt1fx.rgba16.inc.c"
 };
 
 Gfx bob_dl_bob_textures_06000_rgba16_rgba16_aligner[] = {gsSPEndDisplayList()};
@@ -13,9 +13,9 @@ u8 bob_dl_bob_textures_06000_rgba16_rgba16[] = {
 	#include "levels/bob/bob_textures.06000.rgba16.inc.c"
 };
 
-Gfx bob_dl_bob_textures_03800_rgba16_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 bob_dl_bob_textures_03800_rgba16_rgba16[] = {
-	#include "levels/bob/bob_textures.03800.rgba16.inc.c"
+Gfx bob_dl_stone2_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_stone2_rgba16[] = {
+	#include "levels/bob/stone2.rgba16.inc.c"
 };
 
 Gfx bob_dl_bob_textures_03000_rgba16_i8_aligner[] = {gsSPEndDisplayList()};
@@ -28,19 +28,19 @@ u8 bob_dl_bob_textures_04800_rgba16_rgba16[] = {
 	#include "levels/bob/bob_textures.04800.rgba16.inc.c"
 };
 
-Gfx bob_dl_bob_textures_05000_rgba16_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 bob_dl_bob_textures_05000_rgba16_rgba16[] = {
-	#include "levels/bob/bob_textures.05000.rgba16.inc.c"
+Gfx bob_dl_stone3_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_stone3_rgba16[] = {
+	#include "levels/bob/stone3.rgba16.inc.c"
 };
 
-Gfx bob_dl_bob_textures_08800_rgba16_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 bob_dl_bob_textures_08800_rgba16_rgba16[] = {
-	#include "levels/bob/bob_textures.08800.rgba16.inc.c"
+Gfx bob_dl_bob_textures_08800_rgba16_ia8_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_bob_textures_08800_rgba16_ia8[] = {
+	#include "levels/bob/bob_textures.08800.rgba16.ia8.inc.c"
 };
 
-Gfx bob_dl_ttm_textures_04000_rgba16_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 bob_dl_ttm_textures_04000_rgba16_rgba16[] = {
-	#include "levels/bob/ttm_textures.04000.rgba16.inc.c"
+Gfx bob_dl_stone1_i8_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_stone1_i8[] = {
+	#include "levels/bob/stone1.i8.inc.c"
 };
 
 Gfx bob_dl_segment2_12458_rgba16_rgba16_aligner[] = {gsSPEndDisplayList()};
